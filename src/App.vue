ca1d12c57c423dbb41a93b2efc7c761f1b68fc84
Jkose7 <ojosema10@gmail.com>
2024-05-16 11:42:45 -0500

    Exersice numero 2

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
-
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
+import Card from './components/Card.vue'
 
 // Importaciones y logica de js -> parecido a Astro 
 // v-blind -> unir abreviacion ':'
@@ -13,90 +13,61 @@ import { ref, computed } from 'vue'
 // v-show oculta un elemento html solo visualmente 
 // v-for itera sobre un objeto o array con la palabra 'in'
 
-const counter = ref(0)
-const favNumbers = ref([])
-
-const addCounter = () => {
-  counter.value += 1
-}
-
-const removeCounter = () => {
-  counter.value -= 1
-}
-
-const resetCounter = () => {
-  counter.value = 0
-}
-
-const addNumber = () => {
-  favNumbers.value.push(counter.value)
-}
-
-const colorCounter = computed(() => {
-  if (counter.value === 0) return'color:gray'
-
-  return counter.value > 0 ? 'color:green' : 'color:red'
+const teachers = ref([])
+const teacher = ref({
+  name: '',
+  lastName: '',
+  documentation: false
 })
 
-const isNumber = computed(() => {
-  const number = favNumbers.value.find(num => num === counter.value)
-  return number || number === 0
-})
+const addTeacher = () => {
+  teachers.value.push({...teacher.value})
+  console.log(teachers.value)
+}
 
 </script>
 
 <template>
-  <div class="counter" :style="colorCounter">
-    {{ counter }}
-  </div>
-  <div class="container">
-    <div>
-      <button @click="addCounter"> + </button>
-      <button @click="removeCounter"> - </button>
+  <form class="form">
+    <label for="">Name</label>
+    <input type="text" v-model="teacher.name">
+
+    <label for="">LastName</label>
+    <input type="text" v-model="teacher.lastName">
+
+
+    <div class="checkbox">
+      <label for="">Documentation</label>
+      <input type="checkbox" v-model="teacher.documentation">
     </div>
-    <button @click="resetCounter"> reset </button>
-    <button @click="addNumber" :disabled="isNumber"> add </button>
+
+    <button @click.prevent="addTeacher">send</button>
+  </form>
+
+  <div>
+    <h1 v-if="teachers.lenght > 0">List of teachers</h1>
+    <div v-for="te in teachers" :key="te.name">
+      <Card
+        :name="te.name"
+        :lastName="te.lastName"
+        :documentation="te.documentation"
+      />
+    </div>
   </div>
 
-  <div class="list">
-    <h1 v-for="number in favNumbers" :key="number">
-      {{ number }}
-    </h1>
-  </div>
 </template>
 
 <style scoped>
-.counter {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #c7c7c7;
-  height: 50px;
-  font-weight: 900;
-  margin-bottom: 4px;
-}
-
-.container {
+.form {
   display: flex;
   flex-direction: column;
+  gap: 10px;
+}
+
+.checkbox {
   width: 100%;
-  gap: 4px;
-}
-
-.container div {
   display: flex;
-  gap: 4px;
-}
-
-button {
-  border: 0;
-  padding: 1rem 2rem;
-  cursor: pointer;
-  transition: all .3s ease-in-out;
-
-  &:hover {
-    transform: scale(.9);
-    background-color: #c7c7c7
-  }
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,15 @@
+<script setup>
+    defineProps({
+        name: String,
+        lastName: String,
+        documentation: Boolean
+    })
+</script>
+
+<template>
+    <div class="card">
+        <h1>{{ name }}</h1>
+        <p>{{ lastName }}</p>
+        <p>{{ documentation }}</p>
+    </div>
+</template>
