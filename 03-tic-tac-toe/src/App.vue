7699d1ff1f6be37049f045100c0f7a752bb7c20d
Jkose7 <ojosema10@gmail.com>
2024-05-17 08:27:29 -0500

    setup 03 project

---
+++ 03-tic-tac-toe/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const board = ref(Array(9).fill(null))
+
+
+</script>
+
+<template>
+  <h1 class="">TRIQUI</h1>
+</template>
+
+<style scoped>
+
+</style>
