4c55df4ee9738ba232d73c6a10117ca9c31915ab
Jkose7 <ojosema10@gmail.com>
2024-05-23 09:22:01 -0500

    setup auth project

---
+++ 08-auth/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <main class="xl:mx-96">
+    <nav class="w-full py-3 items-center text-lg font-bold flex gap-4">
+      <router-link :to="({ name: 'home' })">Home</router-link>
+      <router-link :to="({ name: 'login' })">Login</router-link>
+    </nav>
+    <router-view></router-view>
+  </main>
+</template>
+
+<style scoped></style>
---
+++ 08-auth/src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1 class="bg-red-600">Home</h1>
+</template>
